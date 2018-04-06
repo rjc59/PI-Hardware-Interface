676b352fde93ec6f26c70d99f21741db5f40e878
Eric Hochendoner <eric.hochendoner@gmail.com>
2018-04-06 16:50:36 -0400

    Update to asynchronous motor driver

--- driver/position.py
+++ driver/position.py
@@ -2,59 +2,138 @@
 
 import socket
 import json
-# from Stepper import stepper
+import _thread as thread # The module is officially named '_thread' in python3 for backwards compatibility reasons.
+from queue import Queue # Thread safe, blocking queue
 
-class Position(object):
+from time import sleep
+haveRPi = False
+try:
+    import RPi.GPIO as gpio
+    haveRPi = True
+except Exception as e:
+    pass
+sleepTime = 0.000001
 
-    def __init__(self, pitch = 0, roll = 0):
-        super(Position, self).__init__()
-        self.stepsPitch = pitch
-        self.stepsRoll = roll
+qRecieve = Queue()
+qSend = Queue()
+
+class Stepper:
+
+    DIR_LEFT = "left"
+    DIR_RIGHT = "right"
+    DIR_NONE = "none"
+    # instantiate stepper 
+    # pins = [stepPin, directionPin, enablePin]
+    def __init__(self, pins):
+        #setup pins
+        self.pins = pins
+        self.stepPin = self.pins[0]
+        self.directionPin = self.pins[1]
+        self.enablePin = self.pins[2]
+        self.stepTime = 0.000001
+        self.position = 0
         
+        if haveRPi:
+            print("haveRPi init")
+            #use the broadcom layout for the gpio
+            gpio.setmode(gpio.BCM)
+            
+            #set gpio pins
+            gpio.setup(self.stepPin, gpio.OUT)
+            gpio.setup(self.directionPin, gpio.OUT)
+            gpio.setup(self.enablePin, gpio.OUT)
+            
+            #set enable to high (i.e. power is NOT going to the motor)
+            gpio.output(self.enablePin, True)
+        
+        print("Stepper initialized (step=" + str(self.stepPin) + ", direction=" + str(self.directionPin) + ", enable=" + str(self.enablePin) + ")")
+    
+    # clears GPIO settings
+    def cleanGPIO(self):
+
+        if haveRPi:
+            print("haveRPi cleanup")
+            gpio.cleanup()
+    
+    # step the motor
+    # dir = direction stepper will move
+    # stayOn = defines whether or not stepper should stay "on" or not. If stepper will need to receive a new step command immediately, this should be set to "True." Otherwise, it should remain at "False."
+    def step(self, dir, stayOn=False):
+
+        #set the output to true for left and false for right
+        turnLeft = True
+        if dir == self.DIR_RIGHT:
+            turnLeft = False;
+            self.position += 1
+        elif dir == self.DIR_LEFT:
+            turnLeft = True
+            self.position -= 1
+        else:
+            print("STEPPER ERROR: invalid direction supplied")
+            return False
+
+        if haveRPi:
+            print("haveRPi step")
+            #set enable to low (i.e. power IS going to the motor)
+            gpio.output(self.enablePin, False)
+            gpio.output(self.directionPin, turnLeft)
+
+            #turning the gpio on and off tells the easy driver to take one step
+            gpio.output(self.stepPin, True)
+            gpio.output(self.stepPin, False)
+
+            if stayOn == False:
+                #set enable to high (i.e. power is NOT going to the motor)
+                gpio.output(self.enablePin, True)
+
 class Device(object):
 
-    def __init__(self, arg):
+    def __init__(self):
 
         super(Device, self).__init__()
-        self.position = Position()
+        self.destination = (0, 0)
         self.rollStepper = Stepper([22, 17, 23]) # Step pin, direction pin, enable pin
-        self.pitchStepper = Stepper([22, 17, 23]) # Step pin, direction pin, enable pin
+        self.pitchStepper = Stepper([24, 18, 25]) # Step pin, direction pin, enable pin
 
     def isSafeMove(self, stepsRoll, stepsPitch):
 
         return True # TODO: Make this work correctly.
 
-    def moveBySteps(self, stepsRoll, stepsPitch):
+    def step(self):
 
-        if not isSafeMove(stepsRoll, stepsPitch):
+        stepsRoll = self.getPosition()[0] - self.destination[0]
+        stepsPitch = self.getPosition()[1] - self.destination[1]
+
+        if not self.isSafeMove(stepsRoll, stepsPitch):
             raise Exception("Movement is not safe to perform.")
 
         rollDirection = ""
-        if stepsRoll > 0: # TODO: Figure out which direction is which.
-            rollDirection = stepper.DIR_LEFT
+        if stepsRoll > 0:
+            self.rollStepper.step(Stepper.DIR_LEFT)
+        elif stepsRoll < 0:
+            self.rollStepper.step(Stepper.DIR_RIGHT)
         else:
-            rollDirection = stepper.DIR_RIGHT
+            pass
 
         pitchDirection = ""
-        if stepsPitch > 0: # TODO: Figure out which direction is which.
-            pitchDirection = stepper.DIR_LEFT
-        else:
-            pitchDirection = stepper.DIR_RIGHT
+        if stepsPitch > 0:
+            self.pitchStepper.step(Stepper.DIR_LEFT)
+        elif stepsPitch < 0:
+            self.pitchStepper.step(Stepper.DIR_RIGHT)
+        else: 
+            pass
 
-        self.rollStepper.step(abs(stepsRoll), rollDirection) #steps, dir, speed, stayOn
-        self.pitchStepper.step(abs(stepsPitch), pitchDirection) #steps, dir, speed, stayOn
+    def startMove(self, position):
 
-    def moveByDegrees(self, degreesRoll, degreesPitch):
-
-        moveBySteps(degreesToSteps(degreesRoll), degreesToSteps(degreesPitch))
-
-    def degreesToSteps(self, degrees):
-
-        return degrees * 100 # TODO: Replace this with the correct multiplier.
+        self.destination = position
 
     def getPosition(self):
 
-        return self.position
+        return (self.rollStepper.position, self.pitchStepper.position)
+
+    def isMoveFinished(self):
+
+        return self.getPosition()[0] == self.destination[0] and self.getPosition()[1] == self.destination[1]
 
 def parseMessage(message):
 
@@ -67,41 +146,74 @@ def parseMessage(message):
 def sendMessage(clientsocket, message):
 
     if isinstance(message, Position):
-        text = json.dumps({'x': (message.stepsPitch), 'y': (message.stepsRoll), 'type': ('absolute')})
+        text = json.dumps({'x': message.stepsPitch, 'y': message.stepsRoll, 'type': 'absolute'})
         clientsocket.send(text)
     else:
         clientsocket.send(message)
 
-def handleConnection(clientsocket, clientAddress, device):
+def threadRecieveCommand():
 
-    # {"x":15,"y":30,"type":"absolute"}
-    message = bytes.decode(clientsocket.recv(2048))
-    while not (message == ""):
-        
-        motionRoll, motionPitch, commandType = parseMessage(message)
-        print("Got message ")
-        print(motionRoll)
-        message = bytes.decode(clientsocket.recv(2048))
-        # if commandType == "steps":
-        #     device.moveBySteps(motionRoll, motionPitch)
-        # elif commandType == "degrees":
-        #     device.moveByDegrees(motionRoll, motionPitch)
-        # elif commandType == "getSteps":
-        #     pos = device.getPosition()
-        #     sendMessage(clientsocket, pos.stepsRoll + "," + pos.stepsPitch)
-        # else:
-        #     raise Exception("unknown commandType")    
-
-def main():
-
-    dev = "" #= Device()
+    # Set up the socket to listen for connections.
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(("localhost", 1337)) # TODO: Decide on a port.
     sock.listen(1)
     while 1:
-        (clientsocket, address) = sock.accept()
-        handleConnection(clientsocket, address, dev)
+        # Receive a connection
+        (recieveSocket, address) = sock.accept()
+
+        # Do everything related to that connection for as long as it's open.
+        message = "not empty"
+        while not (message == ""):
+            # Get the message as a string.
+            message = bytes.decode(recieveSocket.recv(2048)).strip()
+            print("got message " + message)
+            position = parseMessage(message)
+
+            # Put the parsed message into a queue to be consumed by another thread.
+            qRecieve.put(position)
+
+def threadConsumeCommand():
+
+    device = Device()
+    while 1:
+        # Get a command from the queue, the optional True means this call will block until there is something ready to get.
+        message = qRecieve.get(True)
+        device.startMove(message)
+        while not device.isMoveFinished():
+            device.step()
+            # After every step we send an updated position back to the server.
+            qSend.put(device.getPosition())
+            # Sleep until the motor is ready for another step.
+            sleep(sleepTime)
+
+def threadSendPosition():
+
+    # TODO: Open a connection back to the logic engine and send the message
+    while 1:
+        message = qSend.get(True)
+        print("sending message " + str(message))
+        # sendMessage(sendSocket, message)
+
+def main():
+
+    thread.start_new_thread(threadRecieveCommand, ()) # Start new thread requires a function and a tuple as arguments.
+    thread.start_new_thread(threadConsumeCommand, ())
+    thread.start_new_thread(threadSendPosition, ())
+
+    # Spin loop so that the threads can keep running.
+    while 1:
+        pass
+
+
 
 if __name__ == '__main__':
 
     main()
+
+""" Test commands
+{"x":15,"y":30,"type":"absolute"}
+{"x":10,"y":30,"type":"absolute"}
+{"x":10,"y":35,"type":"absolute"}
+{"x":15,"y":25,"type":"absolute"}
+{"x":0,"y":0,"type":"absolute"}
+"""
